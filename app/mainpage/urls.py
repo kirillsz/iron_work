2d3f6f7ae6e2ff4f0b3efe410209d157658a0c32
kirillsz <shakhov2013@list.ru>
2024-03-29 11:37:38 +0300

    Удаляет Django бэкэнд

--- app/mainpage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, order_create
-
-
-urlpatterns = [
-	path('', index),
-    path('api/order/create', order_create, name='order_create'),
-]
--- app/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pip freeze > requirements.txt
-
-python manage.py makemigrations && python manage.py migrate
-
-gunicorn iron_work.wsgi:application -b 0.0.0.0:8000 -w 3 --reload
